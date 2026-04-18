1ad1d5f672be66be3773da2a4be69d5043939e5c
gaddam akhil <gaddamakhil900@gmail.com>
2026-04-18 10:46:29 +0530

    terraform

--- local.tf
+++ local.tf
@@ -8,7 +8,7 @@ locals {
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value  
   alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
   host_header = var.component == "frontend" ? "${var.component}-${var.env}.${var.domain_name}" : "${var.component}.backend_alb-${var.env}.${var.domain_name}"
-  vpc_id = data.aws_ssm_parameter.vpc_id.id
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   common_tags = {
     Project = var.project_name
     Environment = var.env
